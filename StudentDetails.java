1ca561c2dd001e9a5af201136026aed7e38d5e5c
vamsi-3322 <143418147+vamsi-3322@users.noreply.github.com>
2023-08-29 00:09:40 -0700

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,7 @@
+public class StudentDetails {
+    public static void main(String args[]) {
+       System.out.println("Student name = Vamsi");
+       System.out.println("BRANCH = CSM");
+       System.out.println("Regd.No = 22K61A4247");
+      }
+}

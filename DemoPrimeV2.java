60ccd6b4695d02fc4a2d4219d860e3c511547054
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-03 23:18:50 -0700

    Create DemoPrimeV2.java

---
+++ DemoPrimeV2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class DemoPrimeV2 {
+    public static void main(String args[]) {
+      int num,flag=0;
+      System.out.println("Enter integer to check prime or not : ");
+      Scanner sc = new Scanner(System.in);
+      num = sc.nextInt();
+      for(int i=2;i<=num;i++)
+      {
+          
+          if(num%i==0)
+          {
+            System.out.println(num+ " is not a prime number");
+            flag=1;
+            break;
+          }
+      }
+      if(flag==0)
+      {
+          System.out.println(num+ " is a prime number");
+      }
+    }
+}

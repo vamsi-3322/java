fe85b124e1e48b0acac848255f3bd8f82f3d02d6
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-04 00:12:13 -0700

    Update DemoPrime.java

--- DemoPrime.java
+++ DemoPrime.java
@@ -1,7 +1,7 @@
 public class DemoPrime {
     public static void main(String args[]) {
       int num=45,flag=0;
-      for(int i=2;i<=num;i++)
+      for(int i=2;i<num;i++)
       {
           
           if(num%i==0)

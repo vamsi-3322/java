86e482f1c279335d9ddde4fcffbb7ffcda5f8faa
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-03 23:10:00 -0700

    Create DemoPrime.java

---
+++ DemoPrime.java
@@ -0,0 +1,19 @@
+public class DemoPrime {
+    public static void main(String args[]) {
+      int num=45,flag=0;
+      for(int i=2;i<=num;i++)
+      {
+          
+          if(num%i==0)
+          {
+            System.out.println(num+ " is not a prime number");
+            flag=1;
+            break;
+          }
+      }
+      if(flag==0)
+      {
+          System.out.println(num+ " is a prime number");
+      }
+    }
+}

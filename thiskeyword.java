e7a26afbf685843f98de7f23575a8abf769188a0
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-14 15:10:28 +0530

    Create thiskeyword.java

---
+++ thiskeyword.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class Student
+{
+    int rollnumber;
+    String name;
+    float fee;
+    Student(int rollnumber,String name,float fee)
+    {
+        this.rollnumber=rollnumber;
+        this.name=name;
+        this.fee=fee;
+    }
+    void display()
+    {
+        System.out.println("Student details are : ");
+        System.out.println(rollnumber+ " "+name+" "+fee);
+    }
+}
+public class ThisDemo1
+{
+    public static void main(String args[])
+    {
+        Student s1=new Student(4247,"vamsikrishna",47500);
+        Student s2=new Student(4220,"phenhas",48000);
+        s1.display();
+        s2.display();
+    }
+}

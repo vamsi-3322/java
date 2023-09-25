816bd2fd332d66255e6e2300575df9e82eaca296
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-25 11:44:27 +0530

    Create AllAsciiValues.java

---
+++ AllAsciiValues.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class AllAsciiValues
+{
+    public static void main(String args[])
+    {
+        for(int i=30;i<=126;i++)
+        {
+            System.out.println("THE ASCII VALUE OF "+(char)i+ " is "+i);
+        }
+    }
+}

f6d8642574acd3b0a187d7f94546571ac6f4f6b1
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-25 11:34:23 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class AsciiValues
+{
+    public static void main(String args[])
+    {
+        char ch1='a';
+        char ch2='b';
+        int asciivalue1=ch1;
+        int asciivalue2=ch2;
+        System.out.println("The ASCII VALUE of "+ch1+" is " +asciivalue1);
+        System.out.println("The ASCII VALUE of "+ch2+ " is " +asciivalue2);
+    }
+}
+    

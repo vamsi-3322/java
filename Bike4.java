e3657c2a34da48d07b2e6773e6ce49e9da683964
VAMSI KRISHNA <143418147+vamsi-3322@users.noreply.github.com>
2023-09-25 20:52:00 -0700

    Create Bike4.java

---
+++ Bike4.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Bike4
+{
+    final int speedlimit;
+    Bike4()
+    {
+        speedlimit=40;
+        System.out.println("Speed limit is "+speedlimit);
+    }
+    public static void main(String args[])
+    {
+        Bike4 b4=new Bike4();
+    }
+}
